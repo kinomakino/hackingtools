9c6643392da932218664228620c0c095d40eb06a
kinomakino <kinomakino@hotmail.com>
2017-11-29 13:20:54 +0100

    Add files via upload

---
+++ subir-archivos.php
@@ -0,0 +1,59 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1561\cocoasubrtf100
+{\fonttbl\f0\fnil\fcharset0 Monaco;}
+{\colortbl;\red255\green255\blue255;\red68\green133\blue214;\red216\green235\blue255;\red252\green252\blue252;
+\red87\green93\blue94;\red39\green102\blue184;\red189\green217\blue249;\red38\green38\blue38;\red1\green30\blue103;
+\red10\green86\blue216;\red15\green112\blue1;\red0\green0\blue0;\red245\green245\blue245;\red3\green53\blue197;
+\red107\green0\blue109;}
+{\*\expandedcolortbl;;\cssrgb\c32941\c60000\c87059;\cssrgb\c87451\c93725\c100000;\cssrgb\c99216\c99216\c99216;
+\cssrgb\c41569\c43922\c44314;\cssrgb\c19216\c48627\c77255;\cssrgb\c78431\c88235\c98039;\cssrgb\c20000\c20000\c20000;\cssrgb\c0\c17647\c47843;
+\cssrgb\c0\c43529\c87843;\cssrgb\c0\c50196\c0;\cssrgb\c0\c0\c0;\cssrgb\c96863\c96863\c96863;\cssrgb\c0\c30588\c81569;
+\cssrgb\c50196\c0\c50196;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab720
+
+\itap1\trowd \taflags1 \trgaph108\trleft-108 \trcbpat4 \trbrdrt\brdrnil \trbrdrl\brdrnil \trbrdrt\brdrnil \trbrdrr\brdrnil 
+\clvertalt \clcbpat3 \clwWidth488\clftsWidth3 \clbrdrt\brdrnil \clbrdrl\brdrnil \clbrdrb\brdrnil \clbrdrr\brdrnil \clpadl0 \clpadr0 \gaph\cellx4320
+\clvertalt \clshdrawnil \clwWidth15191\clftsWidth3 \clbrdrt\brdrnil \clbrdrl\brdrnil \clbrdrb\brdrnil \clbrdrr\brdrnil \clpadl0 \clpadr0 \gaph\cellx8640
+\pard\intbl\itap1\pardeftab720\sl300\qc\partightenfactor0
+
+\f0\fs24 \cf2 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 1\
+\pard\intbl\itap1\pardeftab720\sl300\qc\partightenfactor0
+\cf6 \cb7 \strokec6 2\
+\pard\intbl\itap1\pardeftab720\sl300\qc\partightenfactor0
+\cf2 \cb1 \strokec2 3\
+\pard\intbl\itap1\pardeftab720\sl300\qc\partightenfactor0
+\cf6 \cb7 \strokec6 4\
+\pard\intbl\itap1\pardeftab720\sl300\qc\partightenfactor0
+\cf2 \cb1 \strokec2 5\
+\pard\intbl\itap1\pardeftab720\sl300\qc\partightenfactor0
+\cf6 \cb7 \strokec6 6\
+\pard\intbl\itap1\pardeftab720\sl300\qc\partightenfactor0
+\cf2 \cb1 \strokec2 7\
+\pard\intbl\itap1\pardeftab720\sl300\qc\partightenfactor0
+\cf6 \cb7 \strokec6 8\
+\pard\intbl\itap1\pardeftab720\sl300\qc\partightenfactor0
+\cf2 \cb1 \strokec2 9\
+\pard\intbl\itap1\pardeftab720\sl300\qc\partightenfactor0
+\cf6 \cb7 \strokec6 10\cell 
+\pard\intbl\itap1\pardeftab720\sl300\partightenfactor0
+\cf8 \cb1 \strokec8 $\cf9 \strokec9 target_path\cf10 \strokec10  = \cf11 \strokec11 "subidas/"\cf8 \strokec8 ;\cf10 \strokec10  \cf12 \strokec12 \
+\pard\intbl\itap1\pardeftab720\sl300\partightenfactor0
+\cf8 \cb13 \strokec8 $\cf9 \strokec9 target_path\cf10 \strokec10  = \cf8 \strokec8 $\cf9 \strokec9 target\cf8 \strokec8 _\cf12 \strokec12 path\cf10 \strokec10  \cf8 \strokec8 .\cf10 \strokec10  \cf14 \strokec14 basename\cf8 \strokec8 (\cf10 \strokec10  \cf8 \strokec8 $\cf9 \strokec9 _FILES\cf8 \strokec8 [\cf11 \strokec11 'archivo-a-subir'\cf8 \strokec8 ][\cf11 \strokec11 'name'\cf8 \strokec8 ]);\cf10 \strokec10  \cf12 \strokec12 \
+\pard\intbl\itap1\pardeftab720\sl300\partightenfactor0
+\cf15 \cb1 \strokec15 if\cf8 \strokec8 (\cf14 \strokec14 move_uploaded_file\cf8 \strokec8 ($\cf9 \strokec9 _FILES\cf8 \strokec8 [\cf11 \strokec11 'archivo-a-subir'\cf8 \strokec8 ][\cf11 \strokec11 'tmp_name'\cf8 \strokec8 ],\cf10 \strokec10  \cf8 \strokec8 $\cf9 \strokec9 target_path\cf8 \strokec8 ))\cf10 \strokec10  \cf12 \strokec12 \
+\pard\intbl\itap1\pardeftab720\sl300\partightenfactor0
+\cf8 \cb13 \strokec8 \{\cf10 \strokec10  \cf12 \strokec12 \
+\pard\intbl\itap1\pardeftab720\sl300\partightenfactor0
+\cf12 \cb1 echo\cf10 \strokec10  \cf11 \strokec11 "El archivo "\cf8 \strokec8 .\cf10 \strokec10  \cf14 \strokec14 basename\cf8 \strokec8 (\cf10 \strokec10  \cf8 \strokec8 $\cf9 \strokec9 _FILES\cf8 \strokec8 [\cf11 \strokec11 'archivo-a-subir'\cf8 \strokec8 ][\cf11 \strokec11 'name'\cf8 \strokec8 ]).\cf11 \strokec11 " ha sido subido exitosamente!"\cf8 \strokec8 ;\cf10 \strokec10  \cf12 \strokec12 \
+\pard\intbl\itap1\pardeftab720\sl300\partightenfactor0
+\cf8 \cb13 \strokec8 \}\cf10 \strokec10  \cf12 \strokec12 \
+\pard\intbl\itap1\pardeftab720\sl300\partightenfactor0
+\cf15 \cb1 \strokec15 else\cf12 \strokec12 \
+\pard\intbl\itap1\pardeftab720\sl300\partightenfactor0
+\cf8 \cb13 \strokec8 \{\cf10 \strokec10  \cf12 \strokec12 \
+\pard\intbl\itap1\pardeftab720\sl300\partightenfactor0
+\cf12 \cb1 echo\cf10 \strokec10  \cf11 \strokec11 "Hubo un error al subir tu archivo! Por favor intenta de nuevo."\cf8 \strokec8 ;\cf10 \strokec10  \cf12 \strokec12 \
+\pard\intbl\itap1\pardeftab720\sl300\partightenfactor0
+\cf8 \cb13 \strokec8 \}\cf12 \strokec12 \cell \lastrow\row
+}
